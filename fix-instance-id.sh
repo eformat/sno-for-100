5b0d0e129a3678ef16692c0f1bcf629cd42788e2
Mike Hepburn <eformat@gmail.com>
2022-11-15 11:52:52 +1000

    🐔 only return running instances 🐔

--- fix-instance-id.sh
+++ fix-instance-id.sh
@@ -31,7 +31,7 @@ find_instance_id() {
     instance_id=$(aws ec2 describe-instances \
     --region=${region} \
     --query "Reservations[].Instances[].InstanceId" \
-    --filters "Name=tag-value,Values=$tag_value" " "Name=instance-state-name,Values=running" \
+    --filters "Name=tag-value,Values=$tag_value" "Name=instance-state-name,Values=running" \
     --output text)
     if [ -z "$instance_id" ]; then
         echo -e "🕱${RED}Failed - could not find instance id associated with tag: $tag_value ?${NC}"
