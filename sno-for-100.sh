36e9232ed7f03830264fc415dfbc214a46bd3cab
Jonkey Guan <jonkey.guan@gmail.com>
2025-09-17 17:41:41 +0800

    fixed for http code 307

--- sno-for-100.sh
+++ sno-for-100.sh
@@ -305,7 +305,7 @@ download_ec2_converter() {
 }
 
 download_openshift_installer() {
-    local ret=$(curl --write-out "%{http_code}" https://mirror.openshift.com/pub/openshift-v4/${system_os_arch}/clients/ocp/${OPENSHIFT_VERSION}/openshift-install-${system_os_flavor}.tar.gz -o ${RUN_DIR}/openshift-install-${system_os_flavor}.tar.gz)
+    local ret=$(curl -L --write-out "%{http_code}" https://mirror.openshift.com/pub/openshift-v4/${system_os_arch}/clients/ocp/${OPENSHIFT_VERSION}/openshift-install-${system_os_flavor}.tar.gz -o ${RUN_DIR}/openshift-install-${system_os_flavor}.tar.gz)
     if [ "$ret" != "200" ]; then
         echo -e "🕱${RED}Failed - to download openshift-install-${system_os_flavor}.tar.gz ?.${NC}"
         return $ret
@@ -319,7 +319,7 @@ download_openshift_installer() {
 }
 
 download_openshift_cli() {
-    local ret=$(curl --write-out "%{http_code}" https://mirror.openshift.com/pub/openshift-v4/${system_os_arch}/clients/ocp/${OPENSHIFT_VERSION}/openshift-client-${system_os_flavor}.tar.gz -o ${RUN_DIR}/openshift-client-${system_os_flavor}.tar.gz)
+    local ret=$(curl -L --write-out "%{http_code}" https://mirror.openshift.com/pub/openshift-v4/${system_os_arch}/clients/ocp/${OPENSHIFT_VERSION}/openshift-client-${system_os_flavor}.tar.gz -o ${RUN_DIR}/openshift-client-${system_os_flavor}.tar.gz)
     if [ "$ret" != "200" ]; then
         echo -e "🕱${RED}Failed - to download openshift-client-${system_os_flavor}.tar.gz ?.${NC}"
         return $ret
